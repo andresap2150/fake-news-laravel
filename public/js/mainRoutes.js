488433d7c27b3e4132f198af088b36e72a59e336
Andres Aparicio <andresap2150@gmail.com>
2019-07-29 22:30:20 -0500

    angular login view created

--- public/js/mainRoutes.js
+++ public/js/mainRoutes.js
@@ -2,8 +2,10 @@ var app = angular.module('mainRoutes',['ngRoute']);
 
 app.config(function ($routeProvider) {
     $routeProvider.when('/',{
-        controller: 'mainController',
-        templateUrl: '../views/main.php'
+        templateUrl: '../views/login.php'
+    }).when('/publish',{
+        templateUrl: '../views/main.php',
+        controller: 'mainController'
     }).when('/about',{
     	templateUrl: '../views/home/about.html'
     }).otherwise({
---
+++ public/views/login.php
@@ -0,0 +1,15 @@
+<div id="login" class="col-md-4 col-md-offset-4"> 
+    <div id="login-form"> 
+        <h4>Login</h4> 
+        <form>
+            <div class="form-group">
+                <input id="username" type="text" /></div> <!--password--> 
+                <div class="form-group">
+                <input id="password" type="password" /></div> <!--submit-->
+            </div>
+            <div class="form-group">
+                <button class="btn btn-primary"type="submit">Login</button>
+            </div> 
+        </form>
+    </div> 
+</div> 
